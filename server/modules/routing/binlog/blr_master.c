02b7bb07785edb6b341bbb788fe0cbee70932d24
Markus Makela <markus.makela@mariadb.com>
2016-04-06 21:35:45 +0300

    Added more information to the incomplete transaction error message
    The error message now logs the caller role, current safe event and the event
    type.

--- server/modules/routing/binlog/blr_master.c
+++ server/modules/routing/binlog/blr_master.c
@@ -1934,9 +1934,11 @@ blr_distribute_binlog_record(ROUTER_INSTANCE *router, REP_HEADER *hdr, uint8_t *
                 if (router->trx_safe)
                 {
                     MXS_ERROR("Slave %s:%d, server ID %u: Sending event from an "
-                              "incomplete transaction from file %s@%u.",
+                              "incomplete transaction from file %s. Slave position: %u "
+                              "Caller role: %s Current safe event: %lu Event type: %x",
                               slave->dcb->remote, ntohs((slave->dcb->ipv4).sin_port),
-                              slave->serverid, slave->binlogfile, slave->binlog_pos);
+                              slave->serverid, slave->binlogfile, slave->binlog_pos,
+                              ROLETOSTR(role), router->current_safe_event, hdr->event_type);
                 }
                 /**
                  * Transaction safety is off or there are no pending transactions
